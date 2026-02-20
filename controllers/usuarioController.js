368779cc1c99d2f10b71175401445658bacb2ba8
sarababe1318 <240755@utxicotepec.edu.mx>
2026-02-20 11:39:18 -0600

    seccion44
    add: Se agrego password y se modificaron campos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -6,5 +6,9 @@ const formularioRegistro = (req,res) =>
 {
     res.render("auth/registro");
 }
+const formularioRecuperacion = (req, res) => {
+    res.render("auth/recuperarPassword", {pagina: "Te ayudamos a recuperar tu contraseña"});
+}
 
-export { formularioLogin, formularioRegistro}
+
+export { formularioLogin, formularioRegistro,formularioRecuperacion}
